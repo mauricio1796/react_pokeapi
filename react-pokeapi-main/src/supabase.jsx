3116021593f478fde479a36ab2274974001c08b3
mauricio1796 <ricardo.mosquerab@uniagustiniana.edu.co>
2025-05-30 15:36:14 +0000

    cambios 1

--- react-pokeapi-main/src/supabase.jsx
+++ react-pokeapi-main/src/supabase.jsx
@@ -1,6 +1,6 @@
 // src/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zabdbjjazhpghuhxrgoo.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphYmRiamphemhwZ2h1aHhyZ29vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxODg1MzEsImV4cCI6MjA2Mjc2NDUzMX0.KJmZ4SXuMUsgiOCq4TvxOoX_4TBdk0U1MLM72Gvfpgg';
+const supabaseUrl = 'https://ixktinlgjjxvnfottfdp.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4a3Rpbmxnamp4dm5mb3R0ZmRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyNzA2NzgsImV4cCI6MjA2Mjg0NjY3OH0.SuOSjG8fy-MoRE88lQONFfwv39IPUc6HqxkCLwTRuFs';
 export const supabase = createClient(supabaseUrl, supabaseKey);
